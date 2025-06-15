f9fab7d9384c8b158ccbaf7d05ced5e57299bed9
Sh Nate <snatekar03@gmail.com>
2025-06-15 20:13:46 +0530

    deploying changes

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'https://featherframe.vercel.app'],
+  origin: [
+    'http://localhost:3000',
+    'https://featherframe.vercel.app',
+    'https://featherframe-frontend.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
